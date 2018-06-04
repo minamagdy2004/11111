e20c7c995c80fd21b3eb7d4ec59fd6328c521e52
BOTQWEREW123 <39924208+BOTQWEREW123@users.noreply.github.com>
2018-06-04 22:48:22 +0300

    Update and rename README.md to bot.js

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ytdl = require('ytdl-core');
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+
+
+client.on('message', message => {
+  if (message.content.startsWith('شين بن نو جتسو مهرجان ليلي يا ليلي')) {
+    const voiceChannel = message.member.voiceChannel;
+    voiceChannel.join()
+      .then(connnection => {
+        const stream = ytdl("https://www.youtube.com/watch?v=R7WRNM9vxzs", { filter: 'audioonly' });
+        const dispatcher = connnection.playStream(stream);
+                dispatcher.on('end'());
+
+      });
+  }
+})
+
+
+client.login('NDE3ODEzMjE5Mzc0MTM3MzQ1.DXYerA.UUW3woMlsTALEQf44dyMwG-j95g');
